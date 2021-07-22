597827a96e1ed66169936cc30fac646a25a41853
leeslatergv <lee.slater@dhsc.gov.uk>
2021-07-23 00:28:41 +0100

    Pipe friendly 3rd version

--- Untitled.R
+++ Untitled.R
@@ -19,7 +19,7 @@ employer_count <- function(df, col_y){
   return(df)   
 }
 
-Q1_Table_function <- employer_count(df, "Q2_No_of_Employees")
+Q1_Table_function1 <- employer_count(df, "Q2_No_of_Employees")
 
 Q1_Table
 Q1_Table_function
@@ -32,5 +32,18 @@ employer_count2 <- function(df, col_y){
     count(.[[col_y]])
   return(df)   
 }
-employer_count2(df, "Q2_No_of_Employees")
+Q1_Table_function2 <- employer_count2(df, "Q2_No_of_Employees")
 
+# This version can run piping through
+employer_count3 <- function(df, col_y, col_z="Q1_Org_Type"){
+  df <- df %>% 
+    filter(.[[col_z]] != "Other") %>% 
+    group_by(Q1_Org_Type) %>% 
+    count(.[[col_y]])
+  return(df)   
+}
+Q1_Table_function3 <- employer_count3(df, "Q2_No_of_Employees")
+
+Q1_Table_function
+Q1_Table_function2
+Q1_Table_function3
